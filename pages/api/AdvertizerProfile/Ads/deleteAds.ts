247124a8a9a5f85b966cc93807d9c7e12980fac5
Mukund <mukund.moona@akqa.com>
2023-02-12 01:18:25 +0530

    deleteads updated

--- pages/api/AdvertizerProfile/Ads/deleteAds.ts
+++ pages/api/AdvertizerProfile/Ads/deleteAds.ts
@@ -10,56 +10,55 @@ type doc = {
   data: any;
 };
 const s3 = new S3({
-  region: "ap-south-1",
-  accessKeyId: "AKIAVBPHLIJHNCGH6DEN",
-  secretAccessKey: "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",
+  region: process.env.REGION,
+  accessKeyId: process.env.ACCESS_KEY,
+  secretAccessKey: process.env.SECRET_KEY,
   signatureVersion: "v4",
 });
 export default async (req: NextApiRequest, res: NextApiResponse<Data>) => {
   const { refId, adId, campaignName, assetUrl } = req.body.data;
-  console.log("asseturl",assetUrl)
-  const bucketkey=assetUrl.split("amazonaws.com/")[1]
+  console.log("asseturl", assetUrl);
+  const bucketkey = assetUrl.split("amazonaws.com/")[1];
   const bucketParams = { Bucket: "exchangeindia", Key: bucketkey };
-  console.log(bucketkey)
-  const deleteObject = await s3.deleteObject(bucketParams,(err,data)=>{
-    if(err) {
-      console.log(err)
-    }else{
-      console.log("successfully deleted")
-      console.log(data)
+  console.log(bucketkey);
+  const deleteObject = await s3.deleteObject(bucketParams, (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("successfully deleted");
+      console.log(data);
     }
-  })
+  });
 
   let doc: doc = await faunaClient.query(
     q.Get(q.Ref(q.Collection("demo_collection"), refId))
   );
-  console.log(campaignName)
+  console.log(campaignName);
 
-  let  campaignData = doc.data.advertizerProfile.campaigns;
+  let campaignData = doc.data.advertizerProfile.campaigns;
   let obj = campaignData.find((x: any) => x.campaignName == campaignName);
-//   console.log("obj",obj)
-  let index = campaignData.indexOf(obj)
-//   console.log("Indexx",index)
+  //   console.log("obj",obj)
+  let index = campaignData.indexOf(obj);
+  //   console.log("Indexx",index)
 
   const updatedData = obj.ads.filter((item: any) => item.adId != adId);
-//   console.log("updated Data", updatedData);
+  //   console.log("updated Data", updatedData);
 
   console.log(campaignData);
-//   console.log(obj.adSlots)
-//   console.log("obj",obj)
-//   console.log(updatedData)
-  obj.ads=updatedData
-//   console.log(obj)
+  //   console.log(obj.adSlots)
+  //   console.log("obj",obj)
+  //   console.log(updatedData)
+  obj.ads = updatedData;
+  //   console.log(obj)
 
+  campaignData.slice(index, 1, obj);
+  //     console.log(pageData[0])
+  //   console.log("pageData",pageData)
+  //   const newData=pageData.filter((item:any)=>item.pageName!=obj.pageName)
 
-    campaignData.slice(index,1,obj)
-//     console.log(pageData[0])
-//   console.log("pageData",pageData)
-//   const newData=pageData.filter((item:any)=>item.pageName!=obj.pageName)
-
-//   newData.push(obj)
-//   console.log("newData",newData)
-    let query = await faunaClient.query(
+  //   newData.push(obj)
+  //   console.log("newData",newData)
+  let query = await faunaClient.query(
     q.Update(q.Ref(q.Collection("demo_collection"), refId), {
       data: {
         advertizerProfile: {
